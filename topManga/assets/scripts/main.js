de2b7635a221475e57f7c9d4c8426daaa73e8099
AdrianHyunam <adriananayadescartes@gmail.com>
2025-03-17 21:02:38 -0600

    Actualizando animaciones y completando paginas.

--- topManga/assets/scripts/main.js
+++ topManga/assets/scripts/main.js
@@ -1,1 +1,40 @@
-console.log("hola mundo");
+let links = document.querySelectorAll('.close');
+
+links.forEach(function(link){
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        
+        let content = document.querySelector('.content');
+
+        content.classList.remove('animate__animated', 'animate__zoomIn');
+
+        content.classList.add('animate__animated', 'animate__zoomOut');
+
+        setTimeout(function(){
+            location.href = "../";
+        },1000);
+
+        return false;
+    });
+});
+
+let irHome = document.querySelectorAll('.ir');
+
+irHome.forEach(function(vamos){
+    vamos.addEventListener("click",function(ev){
+        ev.preventDefault();
+
+        let content = document.querySelector('.content');
+
+        content.classList.remove('animate__animated', 'animate__zoomIn');
+
+        content.classList.add('animate__animated', 'animate__zoomOut');
+
+        setTimeout(function(){
+            location.href = vamos;
+        },1000);
+
+        return false;        
+    });
+
+});
